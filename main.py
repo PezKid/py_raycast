d60b47d29b86036c65395bdee1f698933ce04bd1
PezKid <pezmanmc@gmail.com>
2025-08-01 01:03:01 -0700

    Add full raycasting and 3d rendering functionality.

--- main.py
+++ main.py
@@ -4,24 +4,25 @@ import math
 # Initialize pygame
 pygame.init()
 pygame.display.set_caption("Raycast")
-SCREEN_DIM = SCREEN_WIDTH, SCREEN_HEIGHT = 512, 512
+SCREEN_DIM = SCREEN_WIDTH, SCREEN_HEIGHT = 512*2, 512
 screen = pygame.display.set_mode(SCREEN_DIM)
 clock = pygame.time.Clock()
 
 # Set colors
 PLAYER_COLOR = (255, 0, 0) # red
-MAP_COLOR = (255, 255, 255) # white
 GRID_COLOR = (100, 100, 100) # light gray
-BG_COLOR = (0, 0, 0) # black
+BG_COLOR = (73, 82, 76)
+ONE_COLOR = (121, 128, 123)
+TWO_COLOR = (173, 136, 61)
 
 # Set up map
 map = [
     ['1','1','1','1','1','1','1','1'],
     ['1','0','0','0','0','0','0','1'],
-    ['1','1','1','0','0','0','0','1'],
+    ['1','2','2','0','0','0','0','1'],
     ['1','0','0','0','0','0','0','1'],
-    ['1','0','0','0','0','1','0','1'],
-    ['1','0','1','0','0','1','0','1'],
+    ['1','0','0','0','0','2','0','1'],
+    ['1','0','2','0','0','2','0','1'],
     ['1','0','0','0','0','0','0','1'],
     ['1','1','1','1','1','1','1','1'],
 ]
@@ -47,7 +48,7 @@ class Player:
         dy = self.move_speed * math.sin(self.rotation) * sign
         grid_x = int((self.x + 1.5 * dx) / 64) # collision detection
         grid_y = int((self.y + 1.5 * dy) / 64)
-        if (map[grid_y][grid_x] == '1'):
+        if (map[grid_y][grid_x] != '0'):
             return
         self.x += dx
         self.y += dy
@@ -70,7 +71,7 @@ class Player:
 
     def draw_rays(self):
         distances = []
-        for i in range(-30, 30):
+        for i in range(-32, 32):
             dtheta = i * math.pi / 180
             theta = self.rotation + dtheta
             if theta > 2 * math.pi:
@@ -129,31 +130,39 @@ class Player:
             # Find shortest ray
             hx, hy = h_rayloc
             hdx, hdy = hx - self.x, hy - self.y
-            h_dist = math.sqrt(hdx * hdx + hdy * hdy)
+            h_dist = math.sqrt(hdx * hdx + hdy * hdy) * math.cos(dtheta)
 
             vx, vy = v_rayloc
             vdx, vdy = vx - self.x, vy - self.y
-            v_dist = math.sqrt(vdx * vdx + vdy * vdy)
+            v_dist = math.sqrt(vdx * vdx + vdy * vdy) * math.cos(dtheta)
 
             if h_dist < v_dist:
                 rx, ry = h_rayloc
-                distances.append(h_dist)
+                side = 'H'
+                map = map_at_ray(h_rayloc)
+                color = map_to_color(side, map)
+                distances.append((h_dist, side, map))
             else:
                 rx, ry = v_rayloc
-                distances.append(v_dist)
+                side = 'V'
+                map = map_at_ray(v_rayloc)
+                color = map_to_color(side, map)
+                distances.append((v_dist, side, map))
 
-            pygame.draw.line(screen, (0, 255, 0), (self.x, self.y), (rx, ry), 2)
+            pygame.draw.line(screen, color, (self.x, self.y), (rx, ry), 2)
 
         return distances
 
     def ray_in_wall(self, ray):
-        rx, ry = ray
-        sx = int(rx / sq)
-        sy = int(ry / sq)
-        if sx > 7 or sx < 0 or sy > 7 or sy < 0:
-            return True
-        #print("({}, {})".format(sx, sy))
-        return map[sy][sx] != '0'
+        return map_at_ray(ray) != '0'
+
+def map_at_ray(ray):
+    rx, ry = ray
+    sx = int(rx / sq)
+    sy = int(ry / sq)
+    if sx > 7 or sx < 0 or sy > 7 or sy < 0:
+        return None
+    return map[sy][sx]
 
 # Draws map on screen from variable map where value == 1
 def draw_map():
@@ -164,9 +173,9 @@ def draw_map():
             start_x = w * sq
             start_y = h * sq
 
-            # If map position == 1, draw rect
-            if map[h][w] == '1':
-                pygame.draw.rect(screen, MAP_COLOR, (start_x, start_y, sq, sq))
+            # If map position != 0, draw rect
+            if map[h][w] != '0':
+                pygame.draw.rect(screen, map_to_color('M', map[h][w]), (start_x, start_y, sq, sq))
 
             top_left = (start_x, start_y)
             top_right = (start_x + sq, start_y)
@@ -177,6 +186,41 @@ def draw_map():
             pygame.draw.line(screen, GRID_COLOR, top_left, bottom_left, gridline_width) # left
             pygame.draw.line(screen, GRID_COLOR, top_right, bottom_right, gridline_width) # right
             pygame.draw.line(screen, GRID_COLOR, bottom_left, bottom_right, gridline_width) # bottom
+
+def draw_3d(rays):
+    start_x, start_y = 0, 0
+    for i, ray_info in enumerate(rays):
+        start_x = 512 + i * 8
+        start_y = 0
+        dist, side, map_type = ray_info
+        color = map_to_color(side, map_type)
+
+        slice_height = 64 * SCREEN_HEIGHT / dist
+        start_y = SCREEN_HEIGHT / 2 - slice_height / 2
+        pygame.draw.rect(screen, color, (start_x, start_y, 8, slice_height))
+
+def map_to_color(side, map_type):
+    color = (0, 0, 0)
+    if map_type == '1':
+        color = ONE_COLOR
+    elif map_type == '2':
+        color = TWO_COLOR
+
+    if side == "M":
+        return color
+    elif side == "H":
+        r, g, b = color
+        r = min(r * 1.1, 255)
+        g = min(g * 1.1, 255)
+        b = min(b * 1.1, 255)
+        color = (r, g, b)
+    elif side == "V":
+        r, g, b = color
+        r *= 0.9
+        g *= 0.9
+        b *= 0.9
+        color = (r, g, b)
+    return color
 
 # Initialize player
 player = Player(200, 300, 5, 3, .5, 0.1)
@@ -206,7 +250,8 @@ while playing:
     # Draw screen, map, and player
     screen.fill(BG_COLOR)
     draw_map()
-    print(player.draw_rays())
+    rays = player.draw_rays()
+    draw_3d(rays)
     player.draw()
 
     # Update display
